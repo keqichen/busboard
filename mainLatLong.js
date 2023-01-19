56471c9a71159f8a74009b3a8d28617b8712f908
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-01-19 12:44:35 +0000

    Attempt at postcode api -- IN PROGRESS

--- mainLatLong.js
+++ mainLatLong.js
@@ -1,9 +1,11 @@
-let url = 'https://api.tfl.gov.uk/StopPoint/?lat=51.5539&lon=-0.1468&stopTypes=NaptanPublicBusCoachTram';
+
+// two variables, lat and long to be inserted in url
+let lat = 51.5539;
+let long = -0.1468;
+let url = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${long}&stopTypes=NaptanPublicBusCoachTram`;
 // await version
 let busAPIresponse = await fetch(url);
 let busStopData = await busAPIresponse.json();
 //console.log(busStopData);
 // print the nearest two bus stops
 console.log(`The nearest two bus stops to your location are: ${busStopData.stopPoints[0].commonName} : ${busStopData.stopPoints[0].indicator} and ${busStopData.stopPoints[1].commonName} : ${busStopData.stopPoints[1].indicator}`);
-
-// commonname and indicator
---
+++ mainPostCode.js
@@ -0,0 +1,18 @@
+
+// postcode variable to generate lat and long
+
+//var postCode = "nw51tl"
+//let url = `api.postcodes.io/postcodes/${postCode}`;
+let url = `http://api.postcodes.io/postcodes/nw51tl`;
+let postCodeAPIresponse = await fetch(url);
+let postCodeData = await postCodeAPIresponse.json()
+
+let lat = postCodeData.result.latitude;
+let long = postCodeData.result.longitude;
+let url2 = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${long}&stopTypes=NaptanPublicBusCoachTram`;
+let busAPIresponse = await fetch(url2);
+let busStopData = await busAPIresponse.json();
+//console.log(busStopData);
+// print the nearest two bus stops
+// index 0 and 1 are swapped around compared to mainLatLong.js
+console.log(`The nearest two bus stops to your location are ${busStopData.stopPoints[0].commonName} : ${busStopData.stopPoints[0].indicator} and ${busStopData.stopPoints[1].commonName} : ${busStopData.stopPoints[1].indicator}`);
